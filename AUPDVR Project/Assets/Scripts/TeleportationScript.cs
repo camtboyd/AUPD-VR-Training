5deda4c3089b06b114d09cab21632171c569bfcb
mrose777 <108158126+mrose777@users.noreply.github.com>
2022-12-16 08:55:55 -0500

    Teleportation Script Update
    I added comments to the scripts and attached them to the GameObjects.

--- AUPDVR Project/Assets/Scripts/TeleportationScript.cs
+++ AUPDVR Project/Assets/Scripts/TeleportationScript.cs
@@ -1,58 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour {
-
-    [SerializeField] private GameObject[] objects;
+public class TeleportationScript : MonoBehaviour
+{
+    //is SetActive default true?
+    [SerializeField] private GameObject[] floorSections; //Assuming index follows "elements" in Unity under "Floor"
 
     int currentFloor = 0;
 
-
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         RayLogic();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
     }
 
-    private void RayLogic() {
+    private void RayLogic()
+    {
 
         Ray ray = new Ray(transform.position, Vector3.down);
         RaycastHit hitData; //saving the game object that was hit
         Physics.Raycast(ray, out hitData);
 
-        /* if (hitData.collider.gameObject.tag == "floor") {
+        /*
+         if (hitData.collider.gameObject.tag == "floor") {
               Debug.Log("Hit");
               GameObject o = hitData.collider.gameObject;
               currentFloor = o.GetComponent<FloorProperties>().floorID;
-         }*/
+         }
+        */
 
         //while door is open
         {
-            if (hitData.collider.GetComponent<FloorProperties>().floorID == 0) {
+            if (hitData.collider.GetComponent<FloorProperties>().floorID == 0)
+            {
                 Debug.Log("Bathroom Area");
-                //Turn off bedroom panel
-            } else if (hitData.collider.GetComponent<FloorProperties>().floorID == 1) {
+                floorSections[2].SetActive(false); //Bedroom panel off
+            }
+            else if (hitData.collider.GetComponent<FloorProperties>().floorID == 1)
+            {
                 Debug.Log("Common Space");
                 //All floor panels can stay on 
-            } else if (hitData.collider.GetComponent<FloorProperties>().floorID == 2) {
+            }
+            else if (hitData.collider.GetComponent<FloorProperties>().floorID == 2)
+            {
                 Debug.Log("BedRoom Area");
-                //Turn off bathroom panel 
+                floorSections[0].SetActive(false); //Turn off bathroom panel 
             }
         }
 
         //While the door is closed
         {
-            if (hitData.collider.GetComponent<FloorProperties>().floorID == 0) {
+            if (hitData.collider.GetComponent<FloorProperties>().floorID == 0)
+            {
                 Debug.Log("Bathroom Area");
-                //Turn off bedroom panel 
-            } else if (hitData.collider.GetComponent<FloorProperties>().floorID == 1) {
+                floorSections[2].SetActive(false); //Turn off bedroom panel 
+            }
+            else if (hitData.collider.GetComponent<FloorProperties>().floorID == 1)
+            {
                 Debug.Log("Common Space");
-                //Turn off bedroom panel
+                floorSections[2].SetActive(false); //Turn off bedroom panel
             }
         }
     }
